5925f1ea0c11b35554d8ae8f70900548214075da
mrupyagung <mrrupyagung@gmail.com>
2026-02-24 11:51:06 +0700

    adding video

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -255,7 +255,7 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Priyo and Riski',
+            text: 'The Wedding of Priyo and Ruli',
             dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
             location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
